37ce06062bb81ad0bfb180e8ed4d9f2367d05989
nottings <scott.nottingham@gmail.com>
2014-09-13 12:58:39 -0500

    Updated to reflect Honeywell's new API as of 9/1/2014

--- honeywell_db.py
+++ honeywell_db.py
@@ -3,17 +3,15 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import json
-import urllib
 import datetime
-import re
 import time
-import httplib
+import requests
+
 
 # Login information for Total Connect Comfort web site
 USERNAME="your Total Connect Comfort login id"
 PASSWORD="your total connect comfort password"
-DEVICE_ID=#device id can be obtained by viewing source after logging in to total connect comfort page
+DEVICE_ID=""#device id can be obtained by viewing source after logging in to total connect comfort page
 # it should also be the last part of the URI when visiting the remote control of your thermostat online
 
 # local database username and password
@@ -48,14 +46,14 @@ class Data(Base):
     SchedCoolSp = Column(Float())
     SchedHeatSp = Column(Float())
     ScheduleCapable = Column(Boolean())
-    StatSenseDispTemp = Column(Float())
+    #StatSenseDispTemp = Column(Float())
     StatusCool = Column(Integer())
     StatusHeat = Column(Integer())
     SystemSwitchPosition = Column(Integer())
     TemporaryHoldUntilTime = Column(Integer())
-    Humidity = Column(Integer())
-    Phrase = Column(String(32))
-    Temperature = Column(Integer())
+    #Humidity = Column(Integer())
+    #Phrase = Column(String(32))
+    #Temperature = Column(Integer())
 
 
     def __init__(self, data):
@@ -66,23 +64,24 @@ class Data(Base):
         self.CoolUpperSetptLimit = data['latestData']['uiData']['CoolUpperSetptLimit']
         self.DeviceID = data['latestData']['uiData']['DeviceID']
         self.DispTemperature = data['latestData']['uiData']['DispTemperature']
-        self.DisplayedUnits = data['latestData']['uiData']['DisplayedUnits']
+        self.DisplayedUnits = data['latestData']['uiData']['DisplayUnits']
         self.HeatLowerSetptLimit = data['latestData']['uiData']['HeatLowerSetptLimit']
         self.HeatNextPeriod = data['latestData']['uiData']['HeatNextPeriod']
         self.HeatSetpoint = data['latestData']['uiData']['HeatSetpoint']
         self.HeatUpperSetptLimit = data['latestData']['uiData']['HeatUpperSetptLimit']
         self.IsInVacationHoldMode = data['latestData']['uiData']['IsInVacationHoldMode']
-        self.SchedCoolSp = data['latestData']['uiData']['SchedCoolSp']
-        self.SchedHeatSp = data['latestData']['uiData']['SchedHeatSp']
+        self.SchedCoolSp = data['latestData']['uiData']['ScheduleCoolSp']
+        self.SchedHeatSp = data['latestData']['uiData']['ScheduleHeatSp']
         self.ScheduleCapable = data['latestData']['uiData']['ScheduleCapable']
-        self.StatSenseDispTemp = data['latestData']['uiData']['StatSenseDispTemp']
+        #self.StatSenseDispTemp = data['latestData']['uiData']['StatSenseDispTemp']
         self.StatusCool = data['latestData']['uiData']['StatusCool']
         self.StatusHeat = data['latestData']['uiData']['StatusHeat']
         self.SystemSwitchPosition = data['latestData']['uiData']['SystemSwitchPosition']
         self.TemporaryHoldUntilTime = data['latestData']['uiData']['TemporaryHoldUntilTime']
-        self.Humidity = data['latestData']['weather']['Humidity']
-        self.Phrase = data['latestData']['weather']['Phrase']
-        self.Temperature = data['latestData']['weather']['Temperature']
+        #self.Humidity = data['latestData']['weather']['OutdoorHumidity']
+        #self.Phrase = data['latestData']['weather']['Phrase']
+        #self.Temperature = data['latestData']['weather']['Temperature']
+
 
 #Uncomment this to drop all previous data stored in DB
 # Erases all old data
@@ -98,70 +97,45 @@ AUTH="https://rs.alarmnet.com/TotalConnectComfort/"
 def get_data():
 
     retries=5
-    params=urllib.urlencode({"timeOffset":"240",
-        "UserName":USERNAME,
-        "Password":PASSWORD,
-        "RememberMe":"false"})
-    headers={"Content-Type":"application/x-www-form-urlencoded",
-            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Encoding":"sdch",
-            "Host":"rs.alarmnet.com",
-            "DNT":"1",
-            "Origin":"https://rs.alarmnet.com/TotalComfort/",
-            "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36"
-        }
-    conn = httplib.HTTPSConnection("rs.alarmnet.com")
-    conn.request("POST", "/TotalConnectComfort/",params,headers)
-    r1 = conn.getresponse()
-    cookie = r1.getheader("Set-Cookie")
-    location = r1.getheader("Location")
-    # Strip "expires" "httponly" and "path" from cookie
-    newcookie=cookie
-    #newcookie=re.sub("^expires=[^;]+;","",newcookie)
-    #newcookie=re.sub("^expires=[^;]+$","",newcookie)
-    newcookie=re.sub(";\s*expires=[^;]+","",newcookie)
-    #newcookie=re.sub("^path=[^;]+;","",newcookie)
-    #newcookie=re.sub(";\s*path=[^;]+;",";",newcookie)
-    newcookie=re.sub(";\s*path=[^,]+,",";",newcookie)
-    newcookie=re.sub("HttpOnly\s*[^;],","X;",newcookie)
-    newcookie=re.sub(";\s*HttpOnly\s*,",";",newcookie)
-    cookie=newcookie
-
-    if ((location == None) or (r1.status != 302)):
-        raise BaseException("Login fail" )
-
-    # Skip second query - just go directly to our device_id, rather than letting it
-    # redirect us to it.
-    code=str(DEVICE_ID)
+    auth={
+        'timeOffset': 240,
+        'UserName': USERNAME,
+        'Password': PASSWORD,
+        'RememberMe': 'false'
+    }
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+        'Host': 'rs.alarmnet.com',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://rs.alarmnet.com/TotalConnectComfort'
+    }
+    headers2 = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+        'Host': 'rs.alarmnet.com',
+        'Referer': 'https://rs.alarmnet.com/TotalConnectComfort/Device/Control/'+DEVICE_ID,
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5'
+    }
+    s = requests.Session()
     t = datetime.datetime.now()
     utc_seconds = (time.mktime(t.timetuple()))
     utc_seconds = int(utc_seconds*1000)
-    location="/TotalConnectComfort/Device/CheckDataSession/"+code+"?_="+str(utc_seconds)
-    headers={
-            "Accept":"*/*",
-            "DNT":"1",
-            #"Accept-Encoding":"gzip,deflate,sdch",
-            "Accept-Encoding":"plain",
-            "Cache-Control":"max-age=0",
-            "Accept-Language":"en-US,en,q=0.8",
-            "Connection":"keep-alive",
-            "Host":"rs.alarmnet.com",
-            "Referer":"https://rs.alarmnet.com/TotalConnectComfort/",
-            "X-Requested-With":"XMLHttpRequest",
-            "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36",
-            "Cookie":cookie
-        }
-    conn = httplib.HTTPSConnection("rs.alarmnet.com")
-    #conn.set_debuglevel(999);
-    conn.request("GET", location,None,headers)
-    r3 = conn.getresponse()
-    if (r3.status != 200):
-            print "Bad R3 status ",r3.status, r3.reason
-    #print r3.status, r3.reason
-    rawdata=r3.read()
-    j = json.loads(rawdata)
-    #print json.dumps(j,sort_keys=True,indent=4, separators=(',', ': '))
-    return j
+
+    # Login
+    r = s.post('https://rs.alarmnet.com/TotalConnectComfort', params=auth, headers=headers)
+    r.raise_for_status()
+
+    # Validate
+    r = s.get('https://rs.alarmnet.com/TotalConnectComfort/Device/Control/' + DEVICE_ID, headers=headers)
+    r.raise_for_status()
+
+    # Get Status
+    r = s.get('https://rs.alarmnet.com/TotalConnectComfort/Device/CheckDataSession/' + DEVICE_ID+'?_='+str(utc_seconds), headers=headers2)
+    r.raise_for_status()
+
+    return r.json()
 
 d = Data(get_data())
 session.add(d)
